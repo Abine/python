9639a6996fb6d0955e99f5aa7816350b4f9d9e5d
Tim Stumbaugh <stum@getabine.com>
2013-08-23 16:49:45 -0400

    Port python cookbook to Rightscale Chef
    - State python::default explicitly
    - Compile metadata

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,7 @@ version           "1.3.7"
 depends           "build-essential"
 depends           "yum"
 
-recipe "python", "Installs python, pip, and virtualenv"
+recipe "python::default", "Installs python, pip, and virtualenv"
 recipe "python::package", "Installs python using packages."
 recipe "python::source", "Installs python from source."
 recipe "python::pip", "Installs pip from source."
@@ -17,3 +17,10 @@ recipe "python::virtualenv", "Installs virtualenv using the python_pip resource.
 %w{ debian ubuntu centos redhat fedora freebsd smartos }.each do |os|
   supports os
 end
+
+attribute "python/version",
+  :display_name => "Python version to install",
+  :description => "The version of Python to install. Make sure this is correct",
+  :required => "recommended",
+  :default => "2.7.3",
+  :recipes => ["python::default", "python::package", "python::source"]
